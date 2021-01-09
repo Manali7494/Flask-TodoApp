93356ba653b7f3fc784022fde20e045ff14005e7
Manali7494 <manalibhattacharyya10@gmail.com>
2021-01-09 12:35:05 -0700

    Add todo model

--- app.py
+++ app.py
@@ -12,16 +12,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Creating database
 db = SQLAlchemy(app)
 
+
+# Model Creation
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 # App route 
 @app.route('/')
 
 # Index function
 def index():
+    # query database for todos
+    todo_list = Todo.query.all()
+
+    print(todo_list)
+    
+    # render
     return render_template('base.html') # render template
 
 # Using 'python app.py'
 if __name__ == "__main__":
     #Creating database
     db.create_all()
+
+    #app new to do in database
+    new_todo = Todo(title = "todo 1", complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+
     # Running app in development
     app.run(debug=True)
