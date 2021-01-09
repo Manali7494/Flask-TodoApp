72eff374d3f92585ce3fbea5026a555918fdfe22
Manali7494 <manalibhattacharyya10@gmail.com>
2021-01-09 12:28:37 -0700

    Creating with db

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 # Import of flask
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # Initialization
 app = Flask(__name__) 
+
+#Using sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Creating database
+db = SQLAlchemy(app)
 
 # App route 
 @app.route('/')
@@ -11,6 +19,9 @@ app = Flask(__name__)
 def index():
     return render_template('base.html') # render template
 
-# Using 'python app.py' - Alternate way of running the app using 
+# Using 'python app.py'
 if __name__ == "__main__":
+    #Creating database
+    db.create_all()
+    # Running app in development
     app.run(debug=True)
