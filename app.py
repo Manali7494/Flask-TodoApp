b1835f015ccacb195c7cd6217c79338c4291ee49
Manali7494 <manalibhattacharyya10@gmail.com>
2021-01-09 12:14:29 -0700

    Add about page

--- app.py
+++ app.py
@@ -11,6 +11,14 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
+
+#About Page
+
+@app.route('/about')
+
+def about():
+    return "About Page"
+
 # Using 'python app.py' - Alternate way of running the app using 
 if __name__ == "__main__":
     app.run(debug=True)
